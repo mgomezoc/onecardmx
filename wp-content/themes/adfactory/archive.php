33233d08c41a0ef585ab369b76deeaf024bdc9c7
Cesar Gomez <00.13.zkr@gmail.com>
2025-10-22 10:19:33 -0600

    Refactor theme files to follow WordPress coding standards
    Updated archive.php and functions.php to use WordPress coding standards, including consistent spacing, function formatting, and hook usage. No functional changes were made; this is a code style and readability improvement.

--- wp-content/themes/adfactory/archive.php
+++ wp-content/themes/adfactory/archive.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The template for displaying archive pages
  *
@@ -11,21 +10,21 @@
 get_header();
 ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
-		<?php if (have_posts()) : ?>
+		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
 				<?php
-				the_archive_title('<h1 class="page-title">', '</h1>');
-				the_archive_description('<div class="archive-description">', '</div>');
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
-		<?php
+			<?php
 			/* Start the Loop */
-			while (have_posts()) :
+			while ( have_posts() ) :
 				the_post();
 
 				/*
@@ -33,7 +32,7 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part('template-parts/content', get_post_type());
+				get_template_part( 'template-parts/content', get_post_type() );
 
 			endwhile;
 
@@ -41,13 +40,13 @@ get_header();
 
 		else :
 
-			get_template_part('template-parts/content', 'none');
+			get_template_part( 'template-parts/content', 'none' );
 
 		endif;
 		?>
 
-	</main><!-- #main -->
-</div><!-- #primary -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
 get_sidebar();
